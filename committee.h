8474a3e492af3205264a865c8bad977a9fd3233f
Madeleine Johnson (S) <m_johnson2023@login.mcscn.cs.coloradocollege.edu>
2025-06-11 16:09:16 -0600

    updated header

--- committee.h
+++ committee.h
@@ -2,19 +2,66 @@
 #ifndef COMMITTEE_H
 #define COMMITTEE_H
 
+
+/*!
+ ******************************************************************************
+ * \def COMMITTEE_MAKE_FUNCTION_PTR(name, ret_type, ...)
+ * \brief Makes a function pointer with a given name, return type, and
+ *        parameters
+ * \param name     The name of the function you want to get a pointer to
+ * \param ret_type The return type of the function you want a pointer to
+ * \param ...      A comma separated list of the types of the parameters
+ * 		   to the function you're getting a pointer to
+ ******************************************************************************
+ */
+
+#define COMMITTEE_MAKE_FUNCTION_PTR(name, ret_type, ...) \
+  ret_type (*name)(__VA_ARGS__)
+
+
 /* Allows functions to be visible to all other parts of the library */
 #define COMMITTEE_EXPORT __attribute__((__visibility__("default")))
 
-/*Function that can cancel a library call, and/or change a libray path when a library is called */
+/*!
+ ******************************************************************************
+ *
+ * \fn char* COMMITTEE_EDIT_LIB(char* path);
+ *
+ * \brief allows COMMITTEE to edit a library path 
+ *
+ * \param path  original file path name 
+ *
+ ******************************************************************************
+ */
 
-GOTCHA_EXPORT char* COMMITTEE_EDIT_LIB(char* path);
 
-/*Function that override a given function with a custom version*/
+COMMITTEE_EXPORT char* committee_edit_lib(char* path);
 
-//Testing!!
+/*!
+ ******************************************************************************
+ *
+ * \fn enum void* committee_get_wrappee(committee_wrappee_handle_t)
+ *
+ * \brief Given a COMMITTEE wrapper's handle, returns the wrapped function for it
+ *to call
+ *
+ * \param handle The wrappee handle to return the function pointer for
+ *
+ ******************************************************************************
+ */
+COMMITTEE_EXPORT void *committee_get_wrappee(committee_wrappee_handle_t handle);
+
+/*Function that overrides a built-in function with a custom version*/
+
+COMMITTEE_EXPORT char* committee_edit_func(char* func_name);
 
 /*Function that can bring back original version of overridden function*/
 
-/*Function with ability to monitor function and library calls */
+COMMITTEE_EXPORT char* committee_return_func(char* func_name);
+
+//a later implementation
+/*Function with ability to monitor functions and library calls */
+
+COMMITTEE_EXPORT char* committee_return_func();
 
 #endif
