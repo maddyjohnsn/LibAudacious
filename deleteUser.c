8d4b8832c29a5139253458a9c2b6fd5b50f529fa
Madeleine Johnson (S) <m_johnson2023@login.mcscn.cs.coloradocollege.edu>
2025-06-17 17:04:17 -0600

    started making a list function to hold path names

--- deleteUser.c
+++ deleteUser.c
@@ -1,10 +1,14 @@
-#include "deleteBranchLD.c"
+//#include "deleteBranchLD.c"
+//#include "practiceAud.h"
+#include "practiceAud.c" 
+
 #include <stdio.h> 
 
 int main(){
 	
 	printf("yay!\n");
-
-
+	struct libList myPracStruct;
+       myPracStruct = getLibList();
+       printf("%s\n", myPracStruct.path);
     return 0;
 }
--- practiceAud.c
+++ practiceAud.c
@@ -5,9 +5,24 @@
 #define __USE_GNU
 #include <link.h>
 #include <dlfcn.h>
+#include <inttypes.h>
+//#include "practiceAud.h"
+
+struct libList {
+        char path[100];
+        int size;
+    } ;
+struct libList libList1;
+
+struct libList getLibList(){
+	printf("yes\n");
+	return libList1;
+}
+
 
 unsigned int la_version(unsigned int version){
-        return version;
+	
+	return version;
 
 }
 
@@ -28,25 +43,30 @@ void la_preinit(uintptr_t *cookie) {
 
 unsigned int la_objopen(struct link_map *l, Lmid_t lmid, uintptr_t *cookie) {
         printf("la_objopen called for %s\n", l->l_name);
-        *cookie = (uintptr_t)l; // store link_map as cookie
+	int length = strlen(l->l_name);
+	if(length == 0){
+		printf("it was null\n");
+	}
+	else{
+		printf("here %s\n", l->l_name);
+		strcpy(libList1.path, l->l_name);
+		printf("%s", libList1.path);
+	}
+	*cookie = (uintptr_t)l; // store link_map as cookie
         printf("\n");
 	return LA_FLG_BINDTO | LA_FLG_BINDFROM;
 }
 
-//Elf64_Addr la_x86_64_gnu_pltenter (Elf64_Sym *__sym,unsigned int __ndx,uintptr_t *__refcook,uintptr_t *__defcook,La_x86_64_regs *__regs,unsigned int *__flags,const char *__symname,long int *__framesizep){
-	//printf("in plt\n");
-//     	printf("%s\n",__func__);
-  //  return 0;
-//}
+Elf64_Addr la_x86_64_gnu_pltenter (Elf64_Sym *__sym ,unsigned int __ndx,uintptr_t *__refcook,uintptr_t *__defcook,La_x86_64_regs *__regs,unsigned int *__flags,const char *__symname,long int *__framesizep){
+//	printf("in plt\n");
+  //   	printf("%s\n",__func__);
+    return __sym->st_value;
+}
 
-//unsigned int la_x86_64_gnu_pltexit (Elf64_Sym *__sym,unsigned int __ndx,uintptr_t *__refcook,uintptr_t *__defcook,La_x86_64_regs *inregs,La_x86_64_retval *outregs,const char *__symname){
-  //      printf("in pltex\n");
-//      printf("%s\n",__func__);
-   // return 0;
-//}
 
 uintptr_t la_symbind64(Elf64_Sym *sym, unsigned int ndx, uintptr_t *refcook, uintptr_t *defcook, unsigned int *flags, const char *symname){
 	printf("in symbind\n");
+//	printf(PRIxPTR, sym->st_value);
 	return sym->st_value;
 }
 
